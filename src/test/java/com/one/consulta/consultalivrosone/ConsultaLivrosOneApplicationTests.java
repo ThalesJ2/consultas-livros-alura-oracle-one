3c9876e68ff939de2a31d858afc552521a5c9d43
ThalesJ2 <thalesjorge36@gmail.com>
2025-07-30 17:46:17 -0300

    first commit

---
+++ src/test/java/com/one/consulta/consultalivrosone/ConsultaLivrosOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.consulta.consultalivrosone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaLivrosOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
